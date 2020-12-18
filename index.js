78eb3f1ffbbdb248a6e64fef5c8ed5d993c7c701
Sai Kumar <61982132+saikumar-21@users.noreply.github.com>
2020-12-18 20:16:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 var http = require('http').createServer(app);
 const io = require('socket.io')(http)
-const port = 0.0.0.0;
+const port = process.env.PORT || 3000;
 
 app.use('/static', express.static('static'));
 app.use(express.urlencoded());
